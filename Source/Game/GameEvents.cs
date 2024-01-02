3e7d4b92efa69b449aee6f7571b1a9a092829aed
Mads FS <madsfsoen@gmail.com>
2024-01-02 10:39:35 +0100

    Added a GameEvents class

---
+++ Source/Game/GameEvents.cs
@@ -0,0 +1,17 @@
+﻿#pragma warning disable CA2211 // Non-constant fields should not be visible
+
+using System;
+
+namespace Game
+{
+    public static class GameEvents
+    {
+        public static Action<int> OnScoreChange = delegate { };
+        public static Action<int> OnLevelEnd = delegate { };
+        public static Action OnGamEnd = delegate { };
+
+        public static void BroadcastOnScoreChange(int score) => OnScoreChange.Invoke(score);
+        public static void BroadcastOnLevelEnd(int newLevelIndex) => OnLevelEnd.Invoke(newLevelIndex);
+        public static void BroadcastOnGameEnd() => OnGamEnd.Invoke();
+    }
+}
