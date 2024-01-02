88aeec4efc3d002b82149db1dc13e10470689b4e
Mads FS <madsfsoen@gmail.com>
2024-01-02 10:41:01 +0100

    Represented a Level as a data map of Pellets

---
+++ Source/GameData/PelletMap.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace Game.GameData
+{
+    public class PelletMap
+    {
+        public Pellet[,] Map { get { return map; } }
+        private readonly Pellet[,] map;
+
+        public PelletMap(int height, int width)
+        {
+            map = new Pellet[height, width];
+        }
+
+        public void Add(int x, int y, Pellet pellet) => map[x, y] = pellet;
+
+        public override string ToString()
+        {
+            StringBuilder sb = new();
+            for(int x = 0; x < map.GetLength(0); x++)
+            {
+                for(int y = 0; y < map.GetLength(1); y++)
+                {
+                    sb.Append(map[x, y]);
+                }
+                sb.AppendLine();
+            }
+            return sb.ToString();
+        }
+    }
+}
