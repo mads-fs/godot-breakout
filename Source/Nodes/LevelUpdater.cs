a27ba0163c8f1e49693450908533ffac2b74412d
Mads FS <madsfsoen@gmail.com>
2024-01-02 10:40:28 +0100

    Added node scripts for Score and Level text updates

---
+++ Source/Nodes/LevelUpdater.cs
@@ -0,0 +1,14 @@
+﻿using Godot;
+
+namespace Game.Nodes
+{
+    public partial class LevelUpdater : Node
+    {
+        [Export] private RichTextLabel Label;
+
+        public override void _EnterTree() => GameEvents.OnLevelEnd += HandleOnLevelEnd;
+        public override void _ExitTree() => GameEvents.OnLevelEnd -= HandleOnLevelEnd;
+
+        private void HandleOnLevelEnd(int newLevelIndex) => Label.Text = $"{newLevelIndex + 1}";
+    }
+}
---
+++ Source/Nodes/ScoreUpdater.cs
@@ -0,0 +1,14 @@
+﻿using Godot;
+
+namespace Game.Nodes
+{
+    public partial class ScoreUpdater : Node
+    {
+        [Export] private RichTextLabel Label;
+
+        public override void _EnterTree() => GameEvents.OnScoreChange += HandleOnScoreEvent;
+        public override void _ExitTree() => GameEvents.OnScoreChange -= HandleOnScoreEvent;
+
+        private void HandleOnScoreEvent(int score) => Label.Text = $"{score}";
+    }
+}
