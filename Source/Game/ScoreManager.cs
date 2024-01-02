8a15a627618029b9ee61986d5713af0803748fdb
Mads FS <madsfsoen@gmail.com>
2024-01-02 10:41:18 +0100

    Added a simple ScoreManager

---
+++ Source/Game/ScoreManager.cs
@@ -0,0 +1,14 @@
+﻿namespace Game
+{
+    public class ScoreManager
+    {
+        public int Score { get { return score; } }
+        private int score;
+
+        public void AddScore(int score)
+        {
+            this.score += score;
+            GameEvents.BroadcastOnScoreChange(this.score);
+        }
+    }
+}
