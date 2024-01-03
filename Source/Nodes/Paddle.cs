68bb5aeb5703d14b455cc35c4725b5c12abbd5c8
Mads FS <madsfsoen@gmail.com>
2024-01-04 00:41:08 +0100

    Added code for Firing the ball as well as reset code for when a new level loads

--- Source/Nodes/Paddle.cs
+++ Source/Nodes/Paddle.cs
@@ -6,14 +6,22 @@ namespace Game.Nodes
     {
         [Export] private float speed = 250;
 
+        private Vector2 startPosition;
         private bool isAcceptingInput;
         private float velocity = 0f;
 
         public float GetVelocity() => velocity;
 
+        public override void _EnterTree()
+        {
+            this.startPosition = this.GlobalPosition;
+            GameEvents.OnLevelEnd += this.HandleOnLevelEnd;
+        }
+
         public override void _Process(double delta)
         {
             float horizontal = 0f;
+            if (Input.IsActionJustPressed("fire")) GameEvents.BroadcastOnBallRelease();
             if (Input.IsActionPressed("move_left")) horizontal = Vector2.Left.X;
             if (Input.IsActionPressed("move_right")) horizontal = Vector2.Right.X;
 
@@ -29,5 +37,7 @@ namespace Game.Nodes
             velocity = Mathf.Clamp(velocityDelta, -1, 1f);
             this.MoveAndCollide(new(velocityDelta, 0f));
         }
+
+        private void HandleOnLevelEnd(int newLevelIndex) => this.GlobalPosition = this.startPosition;
     }
 }
