d9884c800c4e8a93a85c16edb8020a5a7a170280
Mads FS <madsfsoen@gmail.com>
2024-01-04 00:41:46 +0100

    Added OnPelletHit event handling in the ScoreManager

--- Source/Game/ScoreManager.cs
+++ Source/Game/ScoreManager.cs
@@ -1,14 +1,25 @@
-﻿namespace Game
+﻿using Game.Nodes;
+
+namespace Game
 {
     public class ScoreManager
     {
         public int Score { get { return score; } }
         private int score;
 
+        public ScoreManager()
+        {
+            GameEvents.OnPelletHit += HandleOnPelletHit;
+            GameEvents.OnGameRestart += HandleOnGameRestart;
+        }
+
         public void AddScore(int score)
         {
             this.score += score;
             GameEvents.BroadcastOnScoreChange(this.score);
         }
+
+        private void HandleOnPelletHit(PelletNode pellet) => AddScore(pellet.Score);
+        private void HandleOnGameRestart() => score = 0;
     }
 }
