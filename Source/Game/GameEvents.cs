11a290a17edad80ddd0de3c885dc4428c87c98f5
Mads FS <madsfsoen@gmail.com>
2024-01-04 00:40:03 +0100

    Added several events to further the game loop

--- Source/Game/GameEvents.cs
+++ Source/Game/GameEvents.cs
@@ -1,17 +1,24 @@
 ﻿#pragma warning disable CA2211 // Non-constant fields should not be visible
 
+using Game.Nodes;
 using System;
 
 namespace Game
 {
     public static class GameEvents
     {
-        public static Action OnPelletHit = delegate { };
+        public static Action OnBallRelease = delegate { };
+        public static Action<PelletNode> OnPelletHit = delegate { };
         public static Action<int> OnScoreChange = delegate { };
+        public static Action OnGameRestart = delegate { };
         public static Action<int> OnLevelEnd = delegate { };
+        public static Action OnGameEnd = delegate { };
 
-        public static void BroadcastOnPelletHit() => OnPelletHit.Invoke();
+        public static void BroadcastOnBallRelease() => OnBallRelease.Invoke();
+        public static void BroadcastOnPelletHit(PelletNode pellet) => OnPelletHit.Invoke(pellet);
         public static void BroadcastOnScoreChange(int score) => OnScoreChange.Invoke(score);
+        public static void BroadcastOnGameRestart() => OnGameRestart.Invoke();
         public static void BroadcastOnLevelEnd(int newLevelIndex) => OnLevelEnd.Invoke(newLevelIndex);
+        public static void BroadcastOnGameEnd() => OnGameEnd.Invoke();
     }
 }
